1c4bcfba6d061f07160a775ab273a6fc1140ac69
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-06-06 19:04:25 -0700

    implement homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionList from '@/components/CompanionList';
+import CTA from '@/components/Cta';
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1 className="">Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="Language"
+          subject="English Literature"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList />
+        <CTA />
+      </section>
+    </main>
   );
 }
 
