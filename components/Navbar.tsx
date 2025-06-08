2eefddf187df2c356b7add6f334aa0d8f7d3de6f
tazman230a <76573669+tazman230a@users.noreply.github.com>
2025-06-08 11:17:17 -0700

    clerk start

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
 import NavItems from "./NavItems"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+
 
 const Navbar = () => {
   return (
@@ -16,8 +18,15 @@ const Navbar = () => {
           </div>
         </Link>
         <div className="flex items-center gap-8">
-         <NavItems />
-         <p>Sign In</p>
+          <NavItems />
+          <SignedOut>
+              <SignInButton>
+                <button className="btn-signin">Sign In</button>
+              </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
     </nav>
   )
